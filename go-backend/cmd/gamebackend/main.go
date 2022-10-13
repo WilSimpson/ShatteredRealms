9ae0e7ceed0286052a313112f02d9c84caafb269
Wil Simpson <simpson.wil255@gmail.com>
2022-10-12 23:05:18 -0500

    Simplified gamebackend main

--- go-backend/cmd/gamebackend/main.go
+++ go-backend/cmd/gamebackend/main.go
@@ -1,44 +1,79 @@
 package main
 
 import (
+	"github.com/WilSimpson/ShatteredRealms/go-backend/pkg/config"
 	"github.com/WilSimpson/ShatteredRealms/go-backend/pkg/helpers"
 	"github.com/WilSimpson/ShatteredRealms/go-backend/pkg/service"
 	log "github.com/sirupsen/logrus"
 	"net"
 	"net/http"
-	"os"
+)
+
+type appConfig struct {
+	GameBackend config.Server `yaml:"gameBackend"`
+	Accounts    config.Server `yaml:"accounts"`
+	Characters  config.Server `yaml:"characters"`
+	KeyDir      string        `yaml:"keyDir"`
+	DBFile      string        `yaml:"dbFile"`
+	Agones      agonesConfig  `yaml:"agones"`
+}
+
+type agonesConfig struct {
+	KeyFile    string        `yaml:"keyFile"`
+	CertFile   string        `yaml:"certFile"`
+	CaCertFile string        `yaml:"caCertFile"`
+	Namespace  string        `yaml:"namespace"`
+	Allocator  config.Server `yaml:"allocator"`
+}
+
+var (
+	conf = &appConfig{
+		GameBackend: config.Server{
+			Port:     8082,
+			Host:     "",
+			Mode:     "development",
+			LogLevel: log.InfoLevel,
+		},
+		Characters: config.Server{
+			Port: 8081,
+			Host: "",
+		},
+		Accounts: config.Server{
+			Port: 8080,
+			Host: "",
+		},
+		KeyDir: "/etc/sro/auth",
+		DBFile: "/etc/sro/db.yaml",
+		Agones: agonesConfig{
+			KeyFile:    "/etc/sro/auth/agones/client/key",
+			CertFile:   "/etc/sro/auth/agones/client/key",
+			CaCertFile: "/etc/sro/auth/agones/ca/ca",
+			Namespace:  "default",
+			Allocator: config.Server{
+				Port: 443,
+				Host: "",
+			},
+		},
+	}
 )
 
 func main() {
 	jwtService, err := service.NewJWTService(conf.KeyDir)
-	if err != nil {
-		log.Errorf("jwt service: %v", err)
-		os.Exit(1)
-	}
+	helpers.Check(err, "jwt service")
 
 	grpcServer, gwmux, err := NewServer(jwtService)
-	if err != nil {
-		log.Errorf("grpc server: %v", err)
-		os.Exit(1)
-	}
+	helpers.Check(err, "create grpc server")
 
-	lis, err := net.Listen("tcp", conf.Address())
-	if err != nil {
-		log.Errorf("listen: %v", err)
-		os.Exit(1)
-	}
+	lis, err := net.Listen("tcp", conf.GameBackend.Address())
+	helpers.Check(err, "listen")
 
 	server := &http.Server{
-		Addr:    conf.Address(),
+		Addr:    conf.GameBackend.Address(),
 		Handler: helpers.GRPCHandlerFunc(grpcServer, gwmux),
 	}
 
 	log.Info("Server starting")
 
 	err = server.Serve(lis)
-
-	if err != nil {
-		log.Errorf("listen: %v", err)
-		os.Exit(1)
-	}
+	helpers.Check(err, "serve")
 }
