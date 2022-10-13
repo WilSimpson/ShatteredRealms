5ef6ca2bf1c705642425e18ca0ebbced4e8d3711
Wil Simpson <simpson.wil255@gmail.com>
2022-10-12 22:56:38 -0500

    Simplified chat main

--- go-backend/cmd/chat/main.go
+++ go-backend/cmd/chat/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"github.com/WilSimpson/ShatteredRealms/go-backend/pkg/config"
+	log "github.com/sirupsen/logrus"
+)
+
+type appConfig struct {
+	Chat     config.Server `yaml:"chat"`
+	Accounts config.Server `yaml:"accounts"`
+	KeyDir   string        `yaml:"keyDir"`
+	DBFile   string        `yaml:"dbFile"`
+}
+
+var (
+	conf = &appConfig{
+		Chat: config.Server{
+			Port:     8180,
+			Host:     "",
+			Mode:     "development",
+			LogLevel: log.InfoLevel,
+		},
+		Accounts: config.Server{
+			Port: 8080,
+			Host: "",
+		},
+		KeyDir: "/etc/sro/auth",
+		DBFile: "/etc/sro/db.yaml",
+	}
+)
+
 func main() {
 
 }
